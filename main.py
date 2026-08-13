7a08477141dc183410b3b2dd5eb895a675235fc7
Charu Malhotra <malhotracharu56@gmail.com>
2026-08-13 16:26:44 +0530

    connecting to github

---
+++ main.py
@@ -0,0 +1,9 @@
+{
+  "name": "insert_edit_into_file",
+  "arguments": {
+    "fileUri": "c:\\Users\\DELL\\projects\\my-first-app\\.vscode\\main.py",
+    "line": 10,
+    "character": 0,
+    "text": "import requests\n\ndef fetch_url(url):\n    response = requests.get(url)\n    return response.json()"
+  }
+}
